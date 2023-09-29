7b03e8e894e660c6a62d651957a7c5e63187621f
npmachine <engineered.design@gmail.com>
2023-09-29 15:03:11 +0900

    feat: add annotation on book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: books
+#
+#  id                 :bigint           not null, primary key
+#  title              :string           default(""), not null
+#  author             :string           default(""), not null
+#  translator         :string           default(""), not null
+#  state              :string           default("draft"), not null
+#  slug               :string           default(""), not null
+#  in_a_nutshell      :text             default(""), not null
+#  description        :text             default(""), not null
+#  published_on       :date             not null
+#  first_published_on :date             not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
 require "rails_helper"
 
 RSpec.describe Book, type: :model do
