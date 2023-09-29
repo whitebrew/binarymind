7b1087ec256b651fa1481af14d63cb4b0fe6fe1e
npmachine <engineered.design@gmail.com>
2023-09-29 15:35:56 +0900

    test: add more tests on book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -24,4 +24,40 @@ RSpec.describe Book, type: :model do
 
   it { expect(book.title).to be_present }
   it { expect(book.author).to be_present }
+  it { expect(book.translator).to be_present }
+  it { expect(book.state).to be_present }
+  it { expect(book.slug).to be_present }
+  it { expect(book.in_a_nutshell).to be_present }
+  it { expect(book.description).to be_present }
+  it { expect(book.published_on).to be_present }
+  it { expect(book.first_published_on).to be_present }
+
+  describe "slug" do
+    it "should be unique" do
+      book1 = create(:book, title: "The Great Gatsby")
+      book2 = create(:book, title: "The Great Gatsby")
+      expect(book1.slug).not_to eq(book2.slug)
+    end
+
+    it "should be parameterized from title" do
+      book = create(:book, title: "The Great Gatsby")
+      expect(book.slug).to eq("the-great-gatsby")
+    end
+  end
+
+  describe "state" do
+    it "should be draft or published" do
+      book = create(:book)
+      expect(book.state).to eq("draft")
+      book.publish
+      expect(book.state).to eq("published")
+    end
+  end
+
+  describe "published_on" do
+    it "should be after first_published_on" do
+      book = create(:book)
+      expect(book.published_on).to be > book.first_published_on
+    end
+  end
 end
