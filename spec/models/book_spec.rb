d8a9d1f8ec4956215441ae1bce89ccb12971e0f5
npmachine <engineered.design@gmail.com>
2023-09-29 17:47:51 +0900

    test: add more tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -20,23 +20,38 @@
 require "rails_helper"
 
 RSpec.describe Book, type: :model do
-  let!(:book) { create(:book) }
+  describe "title" do
+    it "should be present" do
+      book = build(:book, title: nil)
+      expect(book).to be_invalid
+    end
 
-  it { expect(book.title).to be_present }
-  it { expect(book.author).to be_present }
-  it { expect(book.translator).to be_present }
-  it { expect(book.state).to be_present }
-  it { expect(book.slug).to be_present }
-  it { expect(book.in_a_nutshell).to be_present }
-  it { expect(book.description).to be_present }
-  it { expect(book.published_on).to be_present }
-  it { expect(book.first_published_on).to be_present }
+    it "should be unique" do
+      title = Faker::Book.title
+      create(:book, title: title)
+      book = build(:book, title: title)
+      expect(book).to be_invalid
+    end
+  end
+
+  describe "author" do
+    it "should be present" do
+      book = build(:book, author: nil)
+      expect(book).to be_invalid
+    end
+  end
+
+  describe "translator" do
+    it "should be present" do
+      book = build(:book, translator: nil)
+      expect(book).to be_invalid
+    end
+  end
 
   describe "slug" do
-    it "should be unique" do
-      book1 = create(:book, title: "The Great Gatsby")
-      book2 = create(:book, title: "The Great Gatsby")
-      expect(book1.slug).not_to eq(book2.slug)
+    it "should be present" do
+      book = build(:book)
+      expect(book).to be_valid
     end
 
     it "should be parameterized from title" do
@@ -46,15 +61,41 @@ RSpec.describe Book, type: :model do
   end
 
   describe "state" do
+    it "should be present" do
+      book = build(:book, state: nil)
+      expect(book).to be_invalid
+    end
+
     it "should be draft or published" do
+      expect(Book.states).to include("draft", "published")
+    end
+
+    it "sets default value - draft" do
       book = create(:book)
       expect(book.state).to eq("draft")
-      book.publish
-      expect(book.state).to eq("published")
+    end
+  end
+
+  describe "in_a_nutshell" do
+    it "should be present" do
+      book = build(:book, in_a_nutshell: nil)
+      expect(book).to be_invalid
+    end
+  end
+
+  describe "description" do
+    it "should be present" do
+      book = build(:book, description: nil)
+      expect(book).to be_invalid
     end
   end
 
   describe "published_on" do
+    it "should be present" do
+      book = build(:book, published_on: nil)
+      expect(book).to be_invalid
+    end
+
     it "should be after first_published_on" do
       book = create(:book)
       expect(book.published_on).to be > book.first_published_on
